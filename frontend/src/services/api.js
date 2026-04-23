6207a98f30bcab566c86a06eb22d928bfb2bde00
Srinivasa Moorty <nvs.moorty@gmail.com>
2026-04-23 22:28:49 +0530

    added the changes in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,12 +4,11 @@ const API = axios.create({
   baseURL: "https://debate-platform-wzm3.onrender.com/api",
 });
 
-// 🔧 ADD THIS
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = `Bearer ${token}`; // 🔥 IMPORTANT
   }
 
   return req;
